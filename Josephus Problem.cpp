24639e247f6c2bc29d40a64a37d5f06eeb9f1d36
Yucheng Chen <yucheng881111@gmail.com>
2020-04-11 00:35:46 +0800

    josephus 公式

---
+++ Josephus Problem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+//公式: f(n,k)=(f(n-1,k)+k)%n
+int josephus(int n, int k) { //非遞回版本
+	int s = 0; //只有一個人時，一定剩下他(編號0) f(1,k)=0
+	for (int i = 2; i <= n; i++){
+        s = (s + k) % i;
+	}
+
+	return s;
+}
+int josephus_recursion(int n, int k) { //遞回版本
+	return n > 1 ? (josephus_recursion(n - 1, k) + k) % n : 0;
+}
+int main() {
+    cout<<josephus(10,5)<<endl;
+    cout<<josephus_recursion(10,5)<<endl;
+	return 0;
+}
